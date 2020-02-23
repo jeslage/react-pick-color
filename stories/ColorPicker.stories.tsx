2ae029ef276592b7a3c01008e4fca08db3a45160
Johannes Eslage <hello@johanneseslage.de>
2020-02-23 01:58:37 +0100

    Updated stories and screenshot

--- stories/ColorPicker.stories.tsx
+++ stories/ColorPicker.stories.tsx
@@ -9,27 +9,39 @@ export const Init = () => {
   const [color, setColor] = useState("#ffffff");
 
   return (
-    <div style={{ margin: "40px" }}>
-      <ColorPicker
-        color={color}
-        colors={[
-          { r: 0, g: 0, b: 0, a: 0.1 },
-          "red",
-          "blue",
-          "green",
-          "yellow",
-          "crimson",
-          "grey",
-          "lightgrey",
-          "white",
-          "#000",
-          "purple",
-          "papayawhip",
-          "lightgreen",
-          "pink"
-        ]}
-        onChange={color => setColor(color.hex)}
-      />
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <div style={{ margin: "40px" }}>
+        <ColorPicker
+          color={color}
+          disableAlpha
+          onChange={color => setColor(color.hex)}
+        />
+      </div>
+      <div style={{ margin: "40px" }}>
+        <ColorPicker color={color} onChange={color => setColor(color.hex)} />
+      </div>
+      <div style={{ margin: "40px" }}>
+        <ColorPicker
+          color={color}
+          colors={[
+            { r: 0, g: 0, b: 0, a: 0.1 },
+            "red",
+            "blue",
+            "green",
+            "yellow",
+            "crimson",
+            "grey",
+            "lightgrey",
+            "white",
+            "#000",
+            "purple",
+            "papayawhip",
+            "lightgreen",
+            "pink"
+          ]}
+          onChange={color => setColor(color.hex)}
+        />
+      </div>
     </div>
   );
 };
