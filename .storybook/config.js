334d419a4b821f9b7a42d38027968b699c1203f6
Johannes Eslage <hello@johanneseslage.de>
2020-08-26 01:14:09 +0200

    Removed storybook directory

--- .storybook/config.js
+++
@@ -1,7 +0,0 @@
-import { configure } from "@storybook/react";
-
-// automatically import all files ending in *.stories.js
-configure(
-  [require.context("../stories", true, /\.stories\.(ts|tsx)$/)],
-  module
-);
